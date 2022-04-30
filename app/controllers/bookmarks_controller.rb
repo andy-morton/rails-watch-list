c18d2fb76745ef768d3cc9dd229531fb8b18cf73
Andrew Morton <ammorton22@gmail.com>
2022-04-30 16:03:36 +0100

    Finished the back end

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,27 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id]) # needed to instantiate the form_for
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "json"
+require "open-uri"
+
+puts "cleaning up database"
+Movie.destroy_all
+puts "database is clean"
+
+puts "Creating Movies"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+
+
+  movies_serialized = URI.open("#{url}?page=1").read
+  movies = JSON.parse(movies_serialized)['results']
+  movies.each do |movie|
+  base_poster_url = 'https://image.tmdb.org/t/p/original'
+    movie = Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      rating: movie['vote_average'],
+      poster_url: "#{base_poster_url}#{movie['poster_path']}"
+    )
+    puts "movie #{movie.title} is created"
+  end
