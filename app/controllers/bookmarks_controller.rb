83cc24d102eabcbe786cc701b5b691fd51d96887
Andrew Morton <ammorton22@gmail.com>
2022-05-02 15:51:15 +0100

    Minor frontend work including horizontal cards

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,7 @@
 class BookmarksController < ApplicationController
   def new
     @bookmark = Bookmark.new
-    @list = List.find(params[:list_id]) # needed to instantiate the form_for
+    @list = List.find(params[:list_id])
   end
 
   def create
